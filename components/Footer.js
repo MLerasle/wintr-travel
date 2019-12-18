10aa61f24884eac0f1bb96e723a5aa87cc85b899
Maxime Lerasle <maxlerasle@gmail.com>
2019-12-18 19:40:19 +0100

    Responsive footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,36 +7,36 @@ import '../assets/style.css'
 
 const Footer = () => (
   <footer className="flex justify-between items-center bg-primary-blue text-white px-6 py-4 absolute bottom-0 w-full h-10">
-    <section>
+    <section className="hidden md:block">
       <p>© {new Date().getFullYear()} Wintr Travel, All rights reserved.</p>
     </section>
     <section>
       <ul className="flex items-center">
-        <li className="mr-4 tracking-wide">
+        <li className="mr-3 md:mr-4 tracking-wide">
           <Link href="/">
-            <a className="hover:underline">About</a>
+            <a className="text-sm md:text-base hover:underline">About</a>
           </Link>
         </li>
-        <li className="mr-4 tracking-wide">
+        <li className="mr-3 md:mr-4 tracking-wide">
           <Link href="/">
-            <a className="hover:underline">Terms</a>
+            <a className="text-sm md:text-base hover:underline">Terms</a>
           </Link>
         </li>
-        <li className="mr-4 tracking-wide">
+        <li className="mr-3 md:mr-4 tracking-wide">
           <Link href="/">
-            <a className="hover:underline">Privacy</a>
+            <a className="text-sm md:text-base hover:underline">Privacy</a>
           </Link>
         </li>
       </ul>
     </section>
     <section className="flex items-center">
-      <a className="pl-3" href="https://twitter.com">
+      <a className="pl-2 md:pl-3" href="https://twitter.com">
         <Icon path={mdiTwitter} size={1} color="white" />
       </a>
-      <a className="pl-3" href="https://facebook.com">
+      <a className="pl-2 md:pl-3" href="https://facebook.com">
         <Icon path={mdiFacebook} size={1} color="white" />
       </a>
-      <a className="pl-3" href="https://instagram.com">
+      <a className="pl-2 md:pl-3" href="https://instagram.com">
         <Icon path={mdiInstagram} size={1} color="white" />
       </a>
     </section>
