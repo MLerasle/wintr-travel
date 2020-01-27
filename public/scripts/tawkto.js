909c4dc34b19bbe1a2f90f492846afac42299ca9
Maxime Lerasle <maxlerasle@gmail.com>
2020-01-27 20:30:04 +0100

    Use Wintr Travel tawkto account

--- public/scripts/tawkto.js
+++ public/scripts/tawkto.js
@@ -2,10 +2,10 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
   s1.async=true;
-  s1.src='https://embed.tawk.to/5e26d512daaca76c6fcf15f9/1dv42q7e8';
+  s1.src='https://embed.tawk.to/5e2abd298e78b86ed8aadcbb/default';
   s1.charset='UTF-8';
   s1.setAttribute('crossorigin','*');
   s0.parentNode.insertBefore(s1,s0);
 })();
 
-// English version: https://embed.tawk.to/5e26d512daaca76c6fcf15f9/1dv434eb4
+// French script: 'https://embed.tawk.to/5e2abd298e78b86ed8aadcbb/1dvbgidck'
