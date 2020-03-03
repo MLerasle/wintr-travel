40f607d6b38e0a921dead444ea4255942d693a94
Maxime Lerasle <maxlerasle@gmail.com>
2020-03-03 19:46:11 +0100

    Replace axios by isomorphic-unfetch

--- stores/Sagas/Catalog.js
+++ stores/Sagas/Catalog.js
@@ -1,12 +1,13 @@
 import { put } from 'redux-saga/effects'
-import axios from 'axios'
+import fetch from 'isomorphic-unfetch'
 
 import CatalogActions from '../Catalog/Actions'
 
 export function* fetchCatalog() {
   try {
-    const response = yield axios.get('https://catalog.wintr.travel/v1/catalog.json')
-    yield put(CatalogActions.setCatalog(response.data))
+    const response = yield fetch('https://catalog.wintr.travel/v1/catalog.json')
+    const catalog = yield response.json()
+    yield put(CatalogActions.setCatalog(catalog))
   } catch (error) {
     // TODO: Raise an error and catch it somewhere
     console.log('An error occurred during catalog fetching', error)
