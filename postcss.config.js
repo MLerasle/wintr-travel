b80ab97c5b486ea855b9b4aaad03f44cef827207
Maxime Lerasle <maxlerasle@gmail.com>
2020-03-07 14:54:02 +0100

    Handle unused css with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './pages/**/*.js',
+    './components/**/*.js',
+    './node_modules/react-day-picker/**/*.css'
+  ],
+  whitelistPatterns: [/bg-[a-z]|(p|m)(x|y|t|r|b|l|)-[1-9]+/],
+  whitelist: ['md:hidden', 'h-64', 'sm:max-w-xl', 'sm:flex-row', 'sm:items-center', 'sm:mb-0', 'text-3xl', 'text-lg'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/-]+/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    purgecss,
     require('cssnano')
   ]
 }
