e4472b2bc237782f08f854fe59bf5b556897f3e4
Maxime Lerasle <maxime@wintr.travel>
2020-12-09 15:07:30 +0100

    Improve about page

--- pages/about.js
+++ pages/about.js
@@ -32,7 +32,7 @@ const About = () => {
             <div className="flex justify-center items-center">
               <div className="max-w-md mx-8 sm:mx-auto py-10 home-args-desc-1">
                 <h2 className="heading">Qui sommes-nous?</h2>
-                <p className="argument mt-4">
+                <p className="text-gray-600 text-lg leading-loose mt-4">
                   Wintr Travel est une marque audacieuse, qui, depuis 3 ans,
                   façonne en France des équipements pensés pour le grand public.
                   Wintr Travel réinvente l'industrie de l’équipement de ski en
@@ -46,7 +46,7 @@ const About = () => {
             <div className="flex justify-center items-center">
               <div className="max-w-md mx-8 sm:mx-auto py-10 home-args-desc-2">
                 <h2 className="heading">Notre offre</h2>
-                <p className="argument mt-4">
+                <p className="text-gray-600 text-lg leading-loose mt-4">
                   Wintr Travel propose la location clé en main de l’équipement
                   de ski au travers de sa marque{' '}
                   <Link href="/brand">
@@ -54,6 +54,19 @@ const About = () => {
                   </Link>{' '}
                   conçue pour répondre aux exigences des amateurs de skis
                   alliant sécurité, simplicité et nouvelles technologies.
+                </p>
+              </div>
+            </div>
+            <div className="about-image-3 overflow-hidden"></div>
+            <div className="flex justify-center items-center">
+              <div className="max-w-md mx-8 sm:mx-auto py-10 home-args-desc-3">
+                <h2 className="heading">Notre philosophie</h2>
+                <p className="text-gray-600 text-lg leading-loose mt-4">
+                  Notre philosophie est simple : proposer à tous nos clients des
+                  articles de qualité et modernes, quels que soient leur profil
+                  et leur pouvoir d'achat. Nous avons à coeur de vous guider en
+                  toute bienvaillance et de vous donner de la transparence à
+                  toutes les étapes de votre séjour.
                 </p>
               </div>
             </div>
@@ -75,7 +88,9 @@ const About = () => {
               'arg1img'
               'arg1desc'
               'arg2img'
-              'arg2desc';
+              'arg2desc'
+              'arg3img'
+              'arg3desc';
           }
           .about-image-1 {
             grid-area: arg1img;
@@ -89,13 +104,20 @@ const About = () => {
           .home-args-desc-2 {
             grid-area: arg2desc;
           }
+          .about-image-3 {
+            grid-area: arg3img;
+          }
+          .home-args-desc-3 {
+            grid-area: arg3desc;
+          }
 
           @media (min-width: 640px) {
             .grid {
               grid-auto-rows: 350px;
               grid-template-areas:
                 'arg1img arg1desc'
-                'arg2desc arg2img';
+                'arg2desc arg2img'
+                'arg3img arg3desc';
             }
           }
 
