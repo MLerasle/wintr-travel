c5dede6f094aa8312cf7e8343db3d9aa5ed64ec2
Maxime Lerasle <maxime@wintr.travel>
2021-02-02 16:08:40 +0100

    Activate bookings

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import Head from 'next/head';
 
 import MobileImage from '@/UI/MobileImage';
 import Hero from '@/UI/Hero';
-// import BookingForm from '@/App/Home/BookingForm';
-import EmailForm from '@/App/Home/EmailForm';
+import BookingForm from '@/App/Home/BookingForm';
+// import EmailForm from '@/App/Home/EmailForm';
 import HomeArgs from '@/App/Home/Args';
-import Testimonials from '@/App/Home/Testimonials';
+// import Testimonials from '@/App/Home/Testimonials';
 import HomeCta from '@/App/Home/Cta';
 
 import * as gtag from 'lib/gtag';
@@ -28,8 +28,8 @@ const Index = () => {
       </Head>
       <MobileImage />
       <Hero type="full">
-        <EmailForm />
-        {/* <BookingForm /> */}
+        {/* <EmailForm /> */}
+        <BookingForm />
       </Hero>
       <HomeArgs />
       {/* <Testimonials /> */}
