8a269e14f3bc7cba22308c6e03bf67a7d58f8b64
Maxime Lerasle <maxime@wintr.travel>
2021-04-13 12:05:38 +0200

    Better 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,76 +1,60 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Error = () => (
   <>
     <div className="error-container">
-      <p className="error-title">404</p>
-      <p className="error-subtitle">
-        Nous ne parvenons pas à trouver la page que vous recherchez.
+      <Image
+        src="/images/lost.svg"
+        alt="Une voiture se rend au pied des pistes de skis."
+        width={400}
+        height={300}
+        priority={true}
+      />
+      <h1 className="error-header">Vous vous êtes perdus.</h1>
+      <p className="error-message">
+        Laissez nous vous ramener en terrain connu.
       </p>
-      <p className="action">
-        <Link href="/">
-          <a>Retour à la page d'accueil</a>
-        </Link>
-      </p>
+      <Link href="/">
+        <a>Retour à la page d'accueil</a>
+      </Link>
     </div>
     <style jsx>{`
       .error-container {
         width: 100vw;
-        min-height: calc(100vh - 128px);
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-content: center;
         flex-wrap: wrap;
-        background-color: #389469;
-        padding: 0 1rem;
+        background-color: white;
+        padding: 9rem 1rem;
       }
 
-      .error-title,
-      .error-subtitle {
-        color: white;
+      .error-header {
         text-align: center;
+        font-size: 30px;
       }
 
-      .error-title {
-        margin-top: -60px;
-        font-size: 130px;
+      .error-message {
+        text-align: center;
+        font-size: 18px;
       }
 
-      .error-subtitle {
-        margin-top: -20px;
-        font-size: 20px;
-      }
-
-      .action {
+      a {
         margin-top: 3rem;
         font-size: 16px;
         text-align: center;
-        color: #a0aec0;
-      }
-
-      .action > a {
-        color: #389469;
-        background-color: white;
+        color: white;
+        background-color: #389469;
         padding: 1rem;
         border-radius: 0.5rem;
-        font-weight: bold;
         border: 1px solid #389469;
         cursor: pointer;
       }
 
-      .action > a:hover {
-        opacity: 0.97;
-      }
-
-      @media (min-width: 768px) {
-        .error-title {
-          font-size: 230px;
-        }
-
-        .error-subtitle {
-          font-size: 30px;
-        }
+      a:hover {
+        opacity: 0.9;
       }
     `}</style>
   </>
