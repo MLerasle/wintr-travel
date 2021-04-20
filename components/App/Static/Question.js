ef14ab90c5d26c21a90dfd9c6514cb9c98b0d2ed
Maxime Lerasle <maxime@wintr.travel>
2021-04-20 10:24:41 +0200

    FAQ accessibility

--- components/App/Static/Question.js
+++ components/App/Static/Question.js
@@ -1,51 +1,20 @@
-import { useState } from 'react';
+import { Disclosure } from '@headlessui/react';
+import { HiChevronUp } from 'react-icons/hi';
 
-const Question = ({ query, children }) => {
-  const [isOpened, setIsOpened] = useState(false);
-
-  const toggleOpen = () => {
-    setIsOpened(!isOpened);
-  };
-
-  return (
-    <div className="pt-6">
-      <dt className="text-lg">
-        <button
-          type="button"
-          className="text-left w-full flex justify-between items-start text-gray-400"
-          aria-controls="faq-0"
-          aria-expanded="false"
-          onClick={toggleOpen}
-        >
+const Question = ({ query, children }) => (
+  <Disclosure as="div" className="pt-6 text-lg">
+    {({ open }) => (
+      <>
+        <Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
           <span className="font-medium text-gray-900">{query}</span>
-          <span className="ml-6 h-7 flex items-center">
-            <svg
-              className={`rotate ${
-                isOpened ? '-rotate-180' : 'rotate-0'
-              } h-6 w-6 transform`}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M19 9l-7 7-7-7"
-              />
-            </svg>
-          </span>
-        </button>
-      </dt>
-      {isOpened && (
-        <dd className="mt-2 pr-12" id="faq-0">
-          {children}
-        </dd>
-      )}
-    </div>
-  );
-};
+          <HiChevronUp
+            className={`${open ? 'transform rotate-180' : ''} w-6 h-6`}
+          />
+        </Disclosure.Button>
+        <Disclosure.Panel className="mt-2 pr-12">{children}</Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>
+);
 
 export default Question;
