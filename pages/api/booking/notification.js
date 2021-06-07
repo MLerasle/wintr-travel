d13667318ff8c9136f16d07eb274cd168c9a4a5a
Maxime Lerasle <maxime@wintr.travel>
2021-06-07 14:47:33 +0200

    Log to debug

--- pages/api/booking/notification.js
+++ pages/api/booking/notification.js
@@ -10,19 +10,26 @@ import {
 
 export default async (req, res) => {
   try {
+    console.log('Req body in notification', req.body);
     const { message } = req.body;
+    console.log({ message });
     if (!message) {
       throw new Error('No message received');
     }
 
     const booking = extractBookingFromMessage(message);
+    console.log('Booking extracted from message', booking);
 
     await storeBookingInFirestore(booking);
+    console.log('Booking stored in Firestore');
     const customer = await findOrCreateCustomer(booking);
+    console.log('Customer found', customer);
     const invoice = await findOrCreateInvoice(booking);
+    console.log('Invoice found', invoice);
     await updatePaymentIntent(booking.paymentIntentId, {
       customer: customer.id,
     });
+    console.log('Payment intent updated with customer informations');
     await updateBookingInFirestore(booking.paymentIntentId, {
       notification_email_timestamp: Math.floor(Date.now() / 1000),
       stripeCustomerId: customer.id,
@@ -30,10 +37,13 @@ export default async (req, res) => {
       stripeInvoiceUrl: invoice.hosted_invoice_url,
       stripeInvoicePdf: invoice.invoice_pdf,
     });
+    console.log('Booking updated in Firestore');
     await sendBookingConfirmationEmail(booking);
+    console.log('Confirmation email sent');
 
     res.status(201).end();
   } catch (error) {
+    console.log('Error in notification', error);
     Sentry.captureException(error);
     res.status(400).json({
       message: 'Error while sending notification',
