9d6a3e137b955558d49505651f5d4bd8f32a473b
Maxime Lerasle <maxime@wintr.travel>
2023-09-05 09:58:08 +0200

    Remove Google map related code

--- components/App/Booking/BookingFormDeliveryAddress.js
+++ components/App/Booking/BookingFormDeliveryAddress.js
@@ -3,46 +3,46 @@ import useScript from 'react-script-hook';
 
 let autoComplete;
 
-const handleScriptLoad = (updateQuery, updatePlaceId, deliveryAddressRef) => {
-  // Create a circle of 2km radius around Flaine
-  const resortCoordinates = {
-    lat: 46.006538,
-    lng: 6.68953,
-  };
-  const circle = new window.google.maps.Circle({
-    center: resortCoordinates,
-    radius: 2000,
-  });
-  // Assign autoComplete with Google maps places one time
-  autoComplete = new window.google.maps.places.Autocomplete(
-    deliveryAddressRef.current,
-    {
-      origin: new window.google.maps.LatLng(46.006538, 6.68953),
-      bounds: circle.getBounds(),
-      strictBounds: true,
-      componentRestrictions: { country: 'fr' },
-    }
-  );
-  // Specify what properties we will get from API
-  autoComplete.setFields([
-    'address_components',
-    'formatted_address',
-    'name',
-    'place_id',
-  ]);
-  // Add a listener to handle when the place is selected
-  autoComplete.addListener('place_changed', () =>
-    handlePlaceSelect(updateQuery, updatePlaceId)
-  );
-};
+// const handleScriptLoad = (updateQuery, updatePlaceId, deliveryAddressRef) => {
+//   // Create a circle of 2km radius around Flaine
+//   const resortCoordinates = {
+//     lat: 46.006538,
+//     lng: 6.68953,
+//   };
+//   const circle = new window.google.maps.Circle({
+//     center: resortCoordinates,
+//     radius: 2000,
+//   });
+//   // Assign autoComplete with Google maps places one time
+//   autoComplete = new window.google.maps.places.Autocomplete(
+//     deliveryAddressRef.current,
+//     {
+//       origin: new window.google.maps.LatLng(46.006538, 6.68953),
+//       bounds: circle.getBounds(),
+//       strictBounds: true,
+//       componentRestrictions: { country: 'fr' },
+//     }
+//   );
+//   // Specify what properties we will get from API
+//   autoComplete.setFields([
+//     'address_components',
+//     'formatted_address',
+//     'name',
+//     'place_id',
+//   ]);
+//   // Add a listener to handle when the place is selected
+//   autoComplete.addListener('place_changed', () =>
+//     handlePlaceSelect(updateQuery, updatePlaceId)
+//   );
+// };
 
-const handlePlaceSelect = async (updateQuery, updatePlaceId) => {
-  // Get place from google api
-  const addressObject = autoComplete.getPlace();
-  const query = `${addressObject.name} - ${addressObject.formatted_address}`;
-  updatePlaceId(addressObject.place_id);
-  updateQuery(query);
-};
+// const handlePlaceSelect = async (updateQuery, updatePlaceId) => {
+//   // Get place from google api
+//   const addressObject = autoComplete.getPlace();
+//   const query = `${addressObject.name} - ${addressObject.formatted_address}`;
+//   updatePlaceId(addressObject.place_id);
+//   updateQuery(query);
+// };
 
 const BookingFormDeliveryAddress = ({
   booking,
@@ -50,18 +50,18 @@ const BookingFormDeliveryAddress = ({
   token,
   className,
 }) => {
-  useScript({
-    src: `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`,
-    onload: () => handleScriptLoad(setQuery, setPlaceId, deliveryAddressRef),
-    checkForExisting: true,
-  });
+  // useScript({
+  //   src: `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`,
+  //   onload: () => handleScriptLoad(setQuery, setPlaceId, deliveryAddressRef),
+  //   checkForExisting: true,
+  // });
   const [query, setQuery] = useState(booking.deliveryAddress);
   const [placeId, setPlaceId] = useState();
   const deliveryAddressRef = useRef(null);
 
-  useEffect(() => {
-    onDeliveryAddressUpdate(query, placeId);
-  }, [onDeliveryAddressUpdate, query, placeId]);
+  // useEffect(() => {
+  //   onDeliveryAddressUpdate(query, placeId);
+  // }, [onDeliveryAddressUpdate, query, placeId]);
 
   return (
     <>
